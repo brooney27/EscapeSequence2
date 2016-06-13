b7c94871b945604b59b46c90d3a4453b989755d1
brooney27 <brooney278@gmail.com>
2016-06-13 13:28:09 -0400

    Escape

---
+++ src/EscapeSequence2.java
@@ -0,0 +1,13 @@
+
+public class EscapeSequence2 {
+
+	public static void main(String[] args) {
+		System.out.println("Cost\tQuantity\tTotal");
+		System.out.println("$1,000.00\t4\t$4,000.00");
+		System.out.println("$50.00\t\t8\t$400.00");
+		System.out.println("______________________________");
+		System.out.println("TOTAL:\t\t12\t$4,400.00");
+
+	}
+
+}
